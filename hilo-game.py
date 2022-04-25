3e16685c1e6e37074eb3b25d124986d805c1b97e
Bobotato <bhojwanialexander@gmail.com>
2022-04-26 01:41:52 +0800

    Made code PEP8 compliant

--- hilo-game.py
+++ hilo-game.py
@@ -4,10 +4,13 @@ def print_menu():
     print("[2] Ruleset")
     print("[3] Exit")
 
+
 def start_game():
     pass
 
-print("Welcome to Alex's Hi-lo game. It has been in development hell since 2020.")
+
+print("Welcome to Alex's Hi-lo game. "
+      "It has been in development hell since 2020.")
 while True:
     print_menu()
     option = input("Enter your option number: \n")
@@ -15,11 +18,14 @@ while True:
         start_game()
         break
     elif option == "2":
-        print("\nThe game uses a standard 52 card deck. 1 card is dealt face up, and another face down, and you need to guess if the face down card is of a higher or lower value than the face up card.")
-        print("A right answer doubles your bet, and a wrong answer forfeits your bet. Happy playing.\n")
+        print("\nThe game uses a standard 52 card deck. "
+              "1 card is dealt face up, and another face down.\n"
+              "You need to guess if the face down card is of a "
+              "higher or lower value than the face up card.\n"
+              "A right answer doubles your bet, and a wrong "
+              "answer forfeits your bet. Happy playing.\n")
     elif option == "3":
         print("Thanks for playing!")
         quit()
     else:
-         print("Please try typing your choice again.\n")
-
+        print("Please try typing your choice again.\n")
