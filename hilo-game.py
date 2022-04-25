de5fd9523292c3693f4db11a3e7bd2f763d9a4ec
Bobotato <bhojwanialexander@gmail.com>
2022-04-26 01:00:18 +0800

    Added main menu

--- hilo-game.py
+++ hilo-game.py
@@ -1,23 +1,25 @@
-def open_main_menu():
-'''Opens the main menu and its choices'''
-    while True:
-        main_menu_choice = (input("Please type an option or it's shorthand (first letter) to choose:\nPlay\nRules\nQuit\n")).lower()
-        if main_menu_choice == "rules" or main_menu_choice == "r":
-            print("The game is played with a standard 52 card deck. The dealer plays one card face up and one card face down. You have to guess whether the facedown card is of higher or lower value than the faceup card. Getting it right doubles your bet, losing forfeits the bet.\n")
-            open_main_menu()
-        elif main_menu_choice == "quit" or main_menu_choice == "q":
-            print("Thanks for playing!")
-            quit()
-        elif main_menu_choice == "play" or main_menu_choice == "p":
-            print("Let's begin!")
-            game_initialise()
-            break
-        else:
-            print("Try again, remember its \"play/p\" to play, \"rules/r\" for the rules, or \"quit/q\".\n\n")
+def print_menu():
+    '''Prints the main menu'''
+    print("[1] Play")
+    print("[2] Ruleset")
+    print("[3] Exit")
 
-def game_initialise():
-'''Starts the game process'''
-    print("Game is starting...TBC")
+def start_game():
+    pass
 
 print("Welcome to Alex's Hi-lo game. It has been in development hell since 2020.")
-open_main_menu()
+while True:
+    print_menu()
+    option = input("Enter your option number: \n")
+    if option == "1":
+        start_game()
+        break
+    elif option == "2":
+        print("\nThe game uses a standard 52 card deck. 1 card is dealt face up, and another face down, and you need to guess if the face down card is of a higher or lower value than the face up card.")
+        print("A right answer doubles your bet, and a wrong answer forfeits your bet. Happy playing.\n")
+    elif option == "3":
+        print("Thanks for playing!")
+        quit()
+    else:
+         print("Please try typing your choice again.\n")
+
