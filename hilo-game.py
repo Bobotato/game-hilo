63a1c8318dd227cb8bfa1f8dcd13acc2dbe3db14
Bobotato <bhojwanialexander@gmail.com>
2022-04-27 03:15:31 +0800

    Separate deck class into another file deck.py

--- hilo-game.py
+++ hilo-game.py
@@ -1,45 +1,5 @@
 import sys
-from random import shuffle
-
-
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-
-values = ['A', '2', '3', '4', '5', '6', '7',
-          '8', '9', '10', 'J', 'Q', 'K']
-
-
-class Deck:
-    """Deck consisting of playing cards, starts with 52 playing cards"""
-    def __init__(self):
-        self.cards = [Card(value, suit) for suit in suits for value in values]
-
-    def __repr__(self):
-        return f"Deck of {self.count()} cards"
-
-    def count(self):
-        return len(self.cards)
-
-    def restore(self):
-        self.cards = [Card(value, suit) for suit in suits for value in values]
-        return self.cards
-
-    def deal(self, face):
-        if self.count() == 0:
-            raise ValueError("The deck's empty, no cards can "
-                             "be dealt until restored.")
-        else:
-            if face == "up":
-                faceup_card = self.cards.pop()
-                print(f"The face-up card is {faceup_card}")
-                return faceup_card
-            elif face == "down":
-                facedown_card = self.cards.pop()
-                print("The face-down card has been dealt.")
-                return facedown_card
-
-    def shuffle(self):
-        shuffle(self.cards)
-        return self.cards
+import deck
 
 
 class Card:
