d291ec052993720d5c2c4932f9e925ec26ada850
Bobotato <bhojwanialexander@gmail.com>
2022-04-28 00:47:43 +0800

    Removed redundant methods and lines

--- deck.py
+++ deck.py
@@ -22,26 +22,6 @@ class Deck:
     def cards(self, cards):
         self._cards = cards
 
-    def count(self):
-        return len(self.cards)
-
-    def deal(self, face):
-        if self.count() == 0:
-            raise ValueError("The deck's empty, no cards can "
-                             "be dealt until refilled.")
-        else:
-            if face == "up":
-                faceup_card = self.cards.pop()
-                print(f"The face-up card is {faceup_card}")
-                return faceup_card
-            elif face == "down":
-                facedown_card = self.cards.pop()
-                print("The face-down card has been dealt.")
-                return facedown_card
-            else:
-                raise ValueError("Cards can only be dealt "
-                                 "face 'up' or 'down'")
-
     def fill(self):
         suits = card.suits
         values = card.values
@@ -49,9 +29,6 @@ class Deck:
                       SUITS for value in VALUES]
         return self.cards
 
-    def shuffle(self):
-        shuffle(self.cards)
-        return self.cards
-
     def __repr__(self):
         return f"Deck of {self.count()} cards"
+
