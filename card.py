e3cdf7d316f1e2a24fc1eca1aca270338aebbd06
Bobotato <bhojwanialexander@gmail.com>
2022-04-27 20:14:59 +0800

    Added getters and setters

--- card.py
+++ card.py
@@ -1,3 +1,9 @@
+values = ["A", "2", "3", "4", "5", "6", "7",
+          "8", "9", "10", "J", "Q", "K"]
+
+suits = ["D", "H", "C", "S"]
+
+
 class Card:
     """Standard Playing Card Class"""
     def __init__(self, value, suit):
@@ -5,4 +11,27 @@ class Card:
         self.suit = suit
 
     def __repr__(self):
-        return (f"{self.value} of {self.suit}"
+        return (f"{self.value} of {self.suit}")
+
+    @property
+    def value(self, value):
+        return self._value
+
+    @property
+    def suit(self, suit):
+        return self._suit
+
+    @value.setter
+    def value(self, value):
+        if value not in values:
+            raise ValueError("Value must be a part of the 13 values "
+                             "in a standard deck of cards. (A, 2, 3..)")
+        self._value = value
+
+    @suit.setter
+    def suit(self, suit):
+        if suit not in suits:
+            raise ValueError("Suit must be a part of the 4 French suits."
+                             "(H for Hearts, D for Diamonds...)")
+        self._suit = suit
+
