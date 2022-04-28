71121a79df30a08b0375302404ac0533e39be8b5
Alexander Bhojwani <102455514+Bobotato@users.noreply.github.com>
2022-04-28 19:27:34 +0800

    Create deck class (#15)
    * Create deck class

---
+++ deck.py
@@ -0,0 +1,30 @@
+from card import Card, SUITS, VALUES
+
+
+class Deck:
+    """Deck consisting of playing cards,
+    use populate=True to create a 52 card deck on init"""
+    def __init__(self, populate=False):
+        self.cards = []
+        if populate:
+            self.populate()
+
+    @property
+    def cards(self):
+        return self._cards
+
+    @cards.setter
+    def cards(self, cards):
+        for card in cards:
+            if not isinstance(card, Card):
+                raise ValueError("You have tried to insert"
+                                 "an invalid card.")
+        self._cards = cards
+
+    def populate(self):
+        return ([Card(value, suit) for suit in
+                 SUITS for value in VALUES])
+
+    def __repr__(self):
+        return str(self.cards)
+
