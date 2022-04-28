96ed45d83c7aadd07f8bc7f4f249476ac934968d
Bobotato <bhojwanialexander@gmail.com>
2022-04-28 18:05:26 +0800

    Added explicit assignment to __init__ and error if population not boolean

--- deck.py
+++ deck.py
@@ -1,29 +1,31 @@
 from card import Card, SUITS, VALUES
 
+
 class Deck:
     """Deck consisting of playing cards,
-    use fill=True to create a 52 card deck on init"""
-    def __init__(self, populate=False)
+    use populate=True to create a 52 card deck on init"""
+    def __init__(self, cards, populate=False):
+        self.cards = cards
         if populate:
             self.populate()
         elif not populate:
             self.cards = []
-        else:
-            raise ValueError("Please use True or False to choose"
-                             "if the deck starts with 52 cards.")
+        elif not isinstance(populate, bool):
+            raise ValueError("Please use True or False "
+                             "for the population argument.")
 
     @property
     def cards(self):
         return self._cards
 
     @cards.setter
-    def cards(self, cards)
+    def cards(self, cards):
         for card in cards:
-            if isinstance(card,Card):
+            if isinstance(card, Card):
                 continue
             else:
                 raise ValueError("You have tried to insert "
-                                 "an invalid card."
+                                 "an invalid card.")
         self._cards = cards
 
     def populate(self):
