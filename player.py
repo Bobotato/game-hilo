36fe671e93d6718fe77b634633f0d2e6ff25a75c
Bobotato <bhojwanialexander@gmail.com>
2022-04-29 20:43:31 +0800

    Fix issue with wallet balance setter

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 class Player:
     '''A class to represent a player and his wallet'''
-    def __init__(self, wallet_bal = 100):
+    def __init__(self, wallet_bal=100):
         self.wallet_bal = wallet_bal
 
     @property
@@ -8,16 +8,17 @@ class Player:
         return self._wallet_bal
 
     @wallet_bal.setter
-    def wallet_bal(self,wallet_bal):
-        if self.wallet_bal < 0:
-            raise ValueError("A player can't have negative wallet balance.")
+    def wallet_bal(self, wallet_bal):
         self._wallet_bal = wallet_bal
+        if self.wallet_bal < 0:
+            raise ValueError("Player can't have negative wallet balance.")
 
     def bet(self, bet):
-        if self.wallet_bal > bet:
-            raise ValueError("A player can't bet more than the wallet balance.")
+        if self.wallet_bal < bet:
+            raise ValueError("Player can't bet more than the wallet balance.")
         self.wallet_bal -= bet
         return bet
 
     def __repr__(self):
         return f"A player with {self.wallet_bal} credits."
+
