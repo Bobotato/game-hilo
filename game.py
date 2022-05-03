6780de84c0643de57ecf7124007210e8a0bb3788
Bobotato <bhojwanialexander@gmail.com>
2022-05-04 03:06:28 +0800

    Removed all front end functions

--- game.py
+++ game.py
@@ -1,57 +1,27 @@
-from player import Player
-from deck import Deck
-from random import shuffle
-
-
 class Game:
     """This class includes all the methods required for the game of Hilo"""
-    def draw(self, deck, face):
-        if face == "up":
-            faceup = deck.cards.pop()
-            print(faceup)
-            return faceup
-        elif face == "down":
-            facedown = deck.cards.pop()
-            print(facedown)
-            return facedown
+    def draw(self, deck):
+        return deck.cards.pop()
 
-    def take_bet(self, player):
-        while True:
-            bet = int(input("How much would you like to bet?\n"))
-            if bet <= 0:
-                print("Bets cannot be 0 or negative.")
-                continue
-            elif bet > player.credits:
-                print("Bets cannot exceed player's credits.")
-                continue
-            else:
-                break
+    def take_bet(self, player, bet):
+        if bet <= 0:
+            raise ValueError("Bets cannot be 0 or negative.")
+        elif bet > player.credits:
+            raise ValueError("Bets cannot exceed player's credits.")
         player.credits -= bet
-        return bet
 
-    def award_bet(self, player):
+    def award_bet(self, player, bet):
         player.credits += (bet*2)
-
-    def check_guess(self):
-        while True:
-            direction = input("Is the face-down card higher or lower?\n")
-            if direction == "higher":
-                if faceup > facedown:
-                    print("You win")
-                    return True
-                else:
-                    print("You lose")
-                    return False
-
-            elif direction == "lower":
-                if faceup < facedown:
-                    print("You win")
-                    return True
-                else:
-                    print("You lose")
-                    return False
-
+        
+    def check_guess(self, guess, faceup, facedown):
+        if guess == "higher":
+            if faceup > facedown:
+                return True
             else:
-                print("Please input either higher or lower.")
-                continue
+                return False
+        elif guess == "lower":
+            if faceup < facedown:
+                return True
+            else:
+                return False
 
