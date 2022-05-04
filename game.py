0e62f987536aca2f025e672bcadceba905f572cb
Bobotato <bhojwanialexander@gmail.com>
2022-05-04 15:10:29 +0800

    Fix missing self prefixes for variables in game class

--- game.py
+++ game.py
@@ -5,21 +5,21 @@ from player import Player
 class Game:
     """This class includes all the methods required for the game of Hilo"""
     def __init__(self):
-        d = Deck(populate = True)
-        p = Player()
+        self.d = Deck(populate = True)
+        self.p = Player()
 
     def draw(self):
-        return d.cards.pop()
+        return self.d.cards.pop()
 
     def take_bet(self, bet):
         if bet <= 0:
             raise ValueError("Bets cannot be 0 or negative.")
-        elif bet > p.credits:
+        elif bet > self.p.credits:
             raise ValueError("Bets cannot exceed player's credits.")
-        p.credits -= bet
+        self.p.credits -= bet
 
     def award_bet(self, bet):
-        p.credits += (bet*2)
+        self.p.credits += (bet*2)
 
     def check_guess(self, guess, faceup, facedown):
         if guess == "higher":
