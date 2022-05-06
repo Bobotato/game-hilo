1ce70359beffa2a556bf72602b436d2692c2ed60
Bobotato <bhojwanialexander@gmail.com>
2022-05-06 20:00:53 +0800

    Added getters, setters and validation

--- roundinfo.py
+++ roundinfo.py
@@ -1,6 +1,32 @@
+from card import Card
+from player import Player
+
+
 class Roundinfo:
     """This class holds the information for the current round"""
-    def ___init___(self, player, current_card):
+    def __init__(self, player, current_card):
         self.player = player
         self.current_card = current_card
+    
+    @property
+    def current_card(self):
+        return self._current_card
+
+    @current_card.setter
+    def current_card(self, current_card):
+        if not isinstance(current_card, Card):
+            raise ValueError("An invalid card was inserted "
+                             "into the round info.")
+        self._current_card = current_card
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if not isinstance(player, Player):
+            raise ValueError("An invalid player was inserted "
+                             "into the round info.")
+        self._player = player
 
