1e90ab4bc1e591b0b8bc7500b1cdb689217fbdd2
Bobotato <bhojwanialexander@gmail.com>
2022-05-06 16:37:53 +0800

    Create roundinfo class and attributes

---
+++ roundinfo.py
@@ -0,0 +1,7 @@
+class Roundinfo:
+    """This class holds the information for the current round"""
+    def ___init___(self, faceup, facedown, bet, prediction):
+        self.faceup = faceup
+        self.facedown = facedown
+        self.bet = bet
+        self.prediction = prediction
