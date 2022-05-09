5437340955110413355d6e9b2e9682e2c9140dff
Alexander Bhojwani <102455514+Bobotato@users.noreply.github.com>
2022-05-09 16:38:38 +0800

    Create roundinfo class and attributes (#28)
    * Create roundinfo class and attributes

---
+++ roundinfo.py
@@ -0,0 +1,32 @@
+from card import Card
+from player import Player
+
+
+class Roundinfo:
+    """This class holds the information for the current round"""
+    def __init__(self, player, current_card):
+        self.player = player
+        self.current_card = current_card
+    
+    @property
+    def current_card(self):
+        return self._current_card
+
+    @current_card.setter
+    def current_card(self, current_card):
+        if not isinstance(current_card, Card):
+            raise TypeError("current_card must be an instance "
+                            "of a Card")
+        self._current_card = current_card
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if not isinstance(player, Player):
+            raise TypeError("player must be an instance "
+                            "of a Player")
+        self._player = player
+
