8e3a6dd80897cc5658d2c316c273b3a9d0800652
Alexander Bhojwani <102455514+Bobotato@users.noreply.github.com>
2022-05-19 16:39:14 +0800

    Update deck class (#34)
    * Add shuffle param to shuffle deck on init
    
    * Add is_empty method
    
    * Add draw_card method

--- deck.py
+++ deck.py
@@ -1,13 +1,21 @@
+from random import shuffle
+
 from card import Card, SUITS, VALUES
 
 
 class Deck:
-    """Deck consisting of playing cards,
-    use populate=True to create a 52 card deck on init"""
-    def __init__(self, populate=False):
+    """
+    Deck consisting of playing cards,
+    use populate=True to create a 52 card deck,
+    or shuffle=True to shuffle the deck on init.
+    """
+
+    def __init__(self, populate=False, shuffle_deck=False):
         self.cards = []
         if populate:
             self.populate()
+        if shuffle_deck:
+            shuffle(self.cards)
 
     @property
     def cards(self):
@@ -17,14 +25,20 @@ class Deck:
     def cards(self, cards):
         for card in cards:
             if not isinstance(card, Card):
-                raise ValueError("You have tried to insert"
-                                 "an invalid card.")
+                raise ValueError("You have tried to insert" "an invalid card.")
         self._cards = cards
 
+    def draw_card(self):
+        """Draws a card from the deck"""
+        return self.cards.pop()
+
+    def is_empty(self):
+        """Checks if deck is empty"""
+        return len(self.cards) == 0
+
     def populate(self):
-        self.cards = ([Card(value, suit) for suit in
-                      SUITS for value in VALUES])
+        """Populates the deck with 52 cards"""
+        self.cards = [Card(value, suit) for suit in SUITS for value in VALUES]
 
     def __repr__(self):
         return str(self.cards)
-
