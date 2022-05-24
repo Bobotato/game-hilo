6f77a14147d38063ad2fb3c5039288aa76097114
Bobotato <bhojwanialexander@gmail.com>
2022-05-24 15:58:21 +0800

    Convert methods to dunder methods

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ class Game:
         self.player = Player(name)
         self.__current_card = None
 
-    def award_bet(self, bet, *, multiplier=2):
+    def __award_bet__(self, bet, *, multiplier=2):
         """
         Awards a bet to the player
 
@@ -30,7 +30,37 @@ class Game:
         """
         self.player.credits += bet * multiplier
 
-    def is_win(self, drawn_card, prediction):
+    def compute_round_result(self, bet, prediction):
+        """
+        Takes the player's bet and prediction and computes the result of the round.
+
+        :param bet: The player's bet for the current round.
+        :type bet: int
+        :param prediction: The player's prediction for the current round.
+        :type prediction: Prediction
+        :return: An instance of a RoundResult with drawn_card and the result
+                 of the prediction.
+        :rtype: RoundResult
+        """
+        self.__take_bet__(bet)
+        drawn_card = self.deck.draw_card()
+        win = self.__is_win__(drawn_card, prediction)
+
+        if win:
+            self.__award_bet__(bet)
+
+        result = RoundResult(
+            drawn_card,
+            win,
+            is_player_bankrupt=self.player.is_bankrupt(),
+            is_deck_empty=self.deck.is_empty(),
+        )
+
+        self.__current_card = drawn_card
+
+        return result
+
+    def __is_win__(self, drawn_card, prediction):
         """
         Checks if the player won the round.
 
@@ -52,43 +82,19 @@ class Game:
                     "prediction must be an instance of a Prediction"
                 )
 
-    def compute_round_result(self, bet, prediction):
-        """
-        Takes the player's bet and prediction and computes the result of the round.
-
-        :param bet: The player's bet for the current round.
-        :type bet: int
-        :param prediction: The player's prediction for the current round.
-        :type prediction: Prediction
-        :return: An instance of a RoundResult with drawn_card and the result
-                 of the prediction.
-        :rtype: RoundResult
-        """
-        self.take_bet(bet)
-        drawn_card = self.deck.draw_card()
-        win = self.is_win(drawn_card, prediction)
-        if win:
-            self.award_bet(bet)
-        result = RoundResult(
-            drawn_card,
-            win,
-            is_player_bankrupt=self.player.is_bankrupt(),
-            is_deck_empty=self.deck.is_empty(),
-        )
-        self.__current_card = drawn_card
-        return result
-
     def start_round(self):
         """Starts round, drawing current_card and returning
         an instance of Roundinfo"""
         if self.__current_card is None:
             self.__current_card = self.deck.draw_card()
+
         return RoundInfo(self.player, self.__current_card)
 
-    def take_bet(self, bet):
+    def __take_bet__(self, bet):
         """Takes the player's bet and removes it from their account"""
         if bet <= 0:
             raise ValueError("Bets cannot be 0 or negative.")
         elif bet > self.player.credits:
             raise ValueError("Bets cannot exceed player's credits.")
+
         self.player.credits -= bet
