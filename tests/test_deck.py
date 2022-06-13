3df60cd945a3557f28cc804db6961885b6694a19
Bobotato <bhojwanialexander@gmail.com>
2022-06-13 18:54:35 +0800

    Fix test errors

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,74 +1,146 @@
 from random import seed
 
+import pytest
+
 from hilo.models.card import Card
 from hilo.models.deck import Deck
 
-seeded_deck = Deck(populate=False, shuffle_deck=False)
-seeded_deck.cards = [
-    Card("J", "S"),
-    Card("10", "D"),
-    Card("Q", "H"),
-    Card("10", "C"),
-    Card("3", "D"),
-    Card("K", "H"),
-    Card("7", "C"),
-    Card("Q", "D"),
-    Card("10", "H"),
-    Card("6", "D"),
-    Card("4", "H"),
-    Card("K", "S"),
-    Card("5", "S"),
-    Card("3", "C"),
-    Card("J", "C"),
-    Card("8", "S"),
-    Card("9", "S"),
-    Card("9", "C"),
-    Card("2", "S"),
-    Card("A", "S"),
-    Card("K", "D"),
-    Card("Q", "S"),
-    Card("7", "H"),
-    Card("8", "C"),
-    Card("K", "C"),
-    Card("A", "H"),
-    Card("9", "H"),
-    Card("J", "D"),
-    Card("8", "H"),
-    Card("4", "D"),
-    Card("6", "C"),
-    Card("2", "C"),
-    Card("5", "C"),
-    Card("6", "S"),
-    Card("A", "D"),
-    Card("2", "H"),
-    Card("4", "S"),
-    Card("2", "D"),
-    Card("7", "S"),
-    Card("7", "D"),
-    Card("A", "C"),
-    Card("Q", "C"),
-    Card("3", "S"),
-    Card("5", "H"),
-    Card("3", "H"),
-    Card("6", "H"),
-    Card("8", "D"),
-    Card("4", "C"),
-    Card("5", "D"),
-    Card("10", "S"),
-    Card("J", "H"),
-    Card("9", "D"),
-]
+
+@pytest.fixture
+def ordered_deck():
+    return [
+        Card("A", "D"),
+        Card("2", "D"),
+        Card("3", "D"),
+        Card("4", "D"),
+        Card("5", "D"),
+        Card("6", "D"),
+        Card("7", "D"),
+        Card("8", "D"),
+        Card("9", "D"),
+        Card("10", "D"),
+        Card("J", "D"),
+        Card("Q", "D"),
+        Card("K", "D"),
+        Card("A", "C"),
+        Card("2", "C"),
+        Card("3", "C"),
+        Card("4", "C"),
+        Card("5", "C"),
+        Card("6", "C"),
+        Card("7", "C"),
+        Card("8", "C"),
+        Card("9", "C"),
+        Card("10", "C"),
+        Card("J", "C"),
+        Card("Q", "C"),
+        Card("K", "C"),
+        Card("A", "H"),
+        Card("2", "H"),
+        Card("3", "H"),
+        Card("4", "H"),
+        Card("5", "H"),
+        Card("6", "H"),
+        Card("7", "H"),
+        Card("8", "H"),
+        Card("9", "H"),
+        Card("10", "H"),
+        Card("J", "H"),
+        Card("Q", "H"),
+        Card("K", "H"),
+        Card("A", "S"),
+        Card("2", "S"),
+        Card("3", "S"),
+        Card("4", "S"),
+        Card("5", "S"),
+        Card("6", "S"),
+        Card("7", "S"),
+        Card("8", "S"),
+        Card("9", "S"),
+        Card("10", "S"),
+        Card("J", "S"),
+        Card("Q", "S"),
+        Card("K", "S"),
+    ]
 
 
-def test_deck_init_populate():
-    test_deck = Deck(populate=True)
-    assert test_deck.cards != []
+@pytest.fixture
+def seeded_deck():
+    return [
+        Card("J", "S"),
+        Card("10", "D"),
+        Card("Q", "H"),
+        Card("10", "C"),
+        Card("3", "D"),
+        Card("K", "H"),
+        Card("7", "C"),
+        Card("Q", "D"),
+        Card("10", "H"),
+        Card("6", "D"),
+        Card("4", "H"),
+        Card("K", "S"),
+        Card("5", "S"),
+        Card("3", "C"),
+        Card("J", "C"),
+        Card("8", "S"),
+        Card("9", "S"),
+        Card("9", "C"),
+        Card("2", "S"),
+        Card("A", "S"),
+        Card("K", "D"),
+        Card("Q", "S"),
+        Card("7", "H"),
+        Card("8", "C"),
+        Card("K", "C"),
+        Card("A", "H"),
+        Card("9", "H"),
+        Card("J", "D"),
+        Card("8", "H"),
+        Card("4", "D"),
+        Card("6", "C"),
+        Card("2", "C"),
+        Card("5", "C"),
+        Card("6", "S"),
+        Card("A", "D"),
+        Card("2", "H"),
+        Card("4", "S"),
+        Card("2", "D"),
+        Card("7", "S"),
+        Card("7", "D"),
+        Card("A", "C"),
+        Card("Q", "C"),
+        Card("3", "S"),
+        Card("5", "H"),
+        Card("3", "H"),
+        Card("6", "H"),
+        Card("8", "D"),
+        Card("4", "C"),
+        Card("5", "D"),
+        Card("10", "S"),
+        Card("J", "H"),
+        Card("9", "D"),
+    ]
 
 
-def test_deck_init_shuffle():
+def test_deck_init_populate_false_shuffle_false():
+    test_deck = Deck(populate=False, shuffle_deck=False)
+    assert test_deck.cards == []
+
+
+def test_deck_init_populate_true_shuffle_false(ordered_deck):
+    test_deck = Deck(populate=True, shuffle_deck=False)
+    assert test_deck.cards == ordered_deck
+
+
+def test_deck_init_populate_false_shuffle_true():
+    test_deck = Deck(populate=False, shuffle_deck=True)
+    assert test_deck.cards == []
+
+
+def test_deck_init_populate_true_shuffle_true(seeded_deck):
     seed(1)
     dummy_deck = Deck(populate=True, shuffle_deck=True)
-    assert dummy_deck.cards == seeded_deck.cards
+    assert dummy_deck.cards == seeded_deck
 
 
 def test_cards_getter():
@@ -88,13 +160,11 @@ def test_draw_card_return_last_card():
     assert dummy_deck.draw_card() == Card("K", "S")
 
 
-def test_is_empty():
+def test_is_deck_empty():
     dummy_deck = Deck(populate=False, shuffle_deck=False)
+    assert dummy_deck.cards == []
     assert dummy_deck.is_empty()
 
 
-def test_populate():
-    dummy_deck = Deck(populate=False, shuffle_deck=False)
-    dummy_deck.populate()
-    populated_deck = Deck(populate=True, shuffle_deck=False)
-    assert dummy_deck.cards == populated_deck.cards
+def test_generate_full_deck(ordered_deck):
+    assert Deck.generate_full_deck() == ordered_deck
