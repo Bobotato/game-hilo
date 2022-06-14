c01ce22d4e8d0548b770233445d49dc28c5bca65
Bobotato <bhojwanialexander@gmail.com>
2022-06-14 15:06:57 +0800

    Add missing tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hilo.models.player import Player
 
 
@@ -22,6 +24,11 @@ def test_credits_setter():
     assert player.credits == 200
 
 
+def test_credits_setter_negative_credit_raise_ValueError():
+    with pytest.raises(ValueError):
+        Player(name="dummy", credits=-100)
+
+
 def test_name_getter():
     player = Player(name="dummy", credits=100)
     assert player.name == "dummy"
@@ -33,7 +40,23 @@ def test_name_setter():
     assert player.name == "notdummy"
 
 
+def test_name_setter_invalid_name_raise_ValueError():
+    with pytest.raises(ValueError):
+        Player(name="", credits=100)
+
+
 def test_is_player_bankrupt():
     player = Player(name="dummy", credits=0)
-    assert player.credits == 0
-    assert player.is_bankrupt
+    assert player.credits <= 0
+    assert player.is_bankrupt()
+
+
+def test_is_player_not_bankrupt():
+    player = Player(name="dummy", credits=100)
+    assert player.credits > 0
+    assert not player.is_bankrupt()
+
+
+def test_player_repr():
+    player = Player(name="dummy", credits=100)
+    assert repr(player) == "dummy with 100 credits."
