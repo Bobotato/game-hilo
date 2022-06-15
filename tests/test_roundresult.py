6f45ec97f6aa4fab12fb4af353b39e10379ff3a3
Bobotato <bhojwanialexander@gmail.com>
2022-06-15 16:20:56 +0800

    Combine init tests into one test

--- tests/test_roundresult.py
+++ tests/test_roundresult.py
@@ -3,43 +3,16 @@ import pytest
 from hilo.models.roundresult import Card, RoundResult
 
 
-def test_roundresult_init_card():
-    roundresult = RoundResult(
-        drawn_card=Card("A", "H"),
-        win=True,
-        is_player_bankrupt=False,
-        is_deck_empty=False,
-    )
-    assert roundresult.drawn_card == Card("A", "H")
-
-
-def test_roundresult_init_win():
-    roundresult = RoundResult(
-        drawn_card=Card("A", "H"),
-        win=True,
-        is_player_bankrupt=False,
-        is_deck_empty=False,
-    )
-    assert roundresult.win
-
-
-def test_roundresult_init_is_player_bankrupt():
+def test_round_result_init():
     roundresult = RoundResult(
         drawn_card=Card("A", "H"),
         win=True,
         is_player_bankrupt=True,
-        is_deck_empty=False,
-    )
-    assert roundresult.is_player_bankrupt
-
-
-def test_roundresult_init_is_deck_empty():
-    roundresult = RoundResult(
-        drawn_card=Card("A", "H"),
-        win=True,
-        is_player_bankrupt=False,
         is_deck_empty=True,
     )
+    assert roundresult.drawn_card == Card("A", "H")
+    assert roundresult.win
+    assert roundresult.is_player_bankrupt
     assert roundresult.is_deck_empty
 
 
