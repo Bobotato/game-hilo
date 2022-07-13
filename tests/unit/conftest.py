936389285d635eb8e64b67577e4b08e525d793e9
Alexander Bhojwani <102455514+Bobotato@users.noreply.github.com>
2022-07-13 21:58:07 +0800

    Add game test (#58)
    * Add game test
    
    * Add test folder

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,6 @@
-from random import seed
-
 import pytest
 
 from hilo.models.card import Card
-from hilo.models.deck import Deck
 
 
 @pytest.fixture
@@ -120,51 +117,3 @@ def seeded_deck():
         Card("J", "H"),
         Card("9", "D"),
     ]
-
-
-def test_deck_init_populate_false_shuffle_false():
-    test_deck = Deck(populate=False, shuffle_deck=False)
-    assert test_deck.cards == []
-
-
-def test_deck_init_populate_true_shuffle_false(ordered_deck):
-    test_deck = Deck(populate=True, shuffle_deck=False)
-    assert test_deck.cards == ordered_deck
-
-
-def test_deck_init_populate_false_shuffle_true():
-    test_deck = Deck(populate=False, shuffle_deck=True)
-    assert test_deck.cards == []
-
-
-def test_deck_init_populate_true_shuffle_true(seeded_deck):
-    seed(1)
-    dummy_deck = Deck(populate=True, shuffle_deck=True)
-    assert dummy_deck.cards == seeded_deck
-
-
-def test_cards_getter():
-    dummy_deck = Deck(populate=False, shuffle_deck=False)
-    dummy_deck.cards.append(Card("A", "H"))
-    assert dummy_deck.cards == [Card("A", "H")]
-
-
-def test_cards_setter():
-    dummy_deck = Deck(populate=False, shuffle_deck=False)
-    dummy_deck.cards = [Card("A", "H")]
-    assert dummy_deck.cards == [Card("A", "H")]
-
-
-def test_draw_card_return_last_card():
-    dummy_deck = Deck(populate=True, shuffle_deck=False)
-    assert dummy_deck.draw_card() == Card("K", "S")
-
-
-def test_is_deck_empty():
-    dummy_deck = Deck(populate=False, shuffle_deck=False)
-    assert dummy_deck.cards == []
-    assert dummy_deck.is_empty()
-
-
-def test_generate_full_deck(ordered_deck):
-    assert Deck.generate_full_deck() == ordered_deck
