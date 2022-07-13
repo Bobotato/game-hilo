ddc8bcf666d484fe04aadc1f5cb5b815bfcf67f4
Bobotato <bhojwanialexander@gmail.com>
2022-07-13 20:19:02 +0800

    Remove unnecessary checks and improve spacing

--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -4,18 +4,12 @@ import pytest
 
 from hilo.game import Game, Prediction
 from hilo.models.card import Card
-from hilo.models.deck import Deck
-from hilo.models.player import Player
-from hilo.models.roundinfo import RoundInfo
-from hilo.models.roundresult import RoundResult
 
 
 def test_game_init(seeded_deck):
     seed(1)
     game = Game("test")
-    assert isinstance(game.deck, Deck)
     assert game.deck.cards == seeded_deck
-    assert isinstance(game.player, Player)
     assert game.player.name == "test"
     assert game._Game__current_card is None
 
@@ -23,13 +17,15 @@ def test_game_init(seeded_deck):
 def test_award_bet():
     game = Game("test")
     assert game.player.credits == 100
+
     game._Game__award_bet(bet=100, multiplier=5)
     assert game.player.credits == 600
 
 
-def test_award_bet_default():
+def test_award_bet_multiplier_default():
     game = Game("test")
     assert game.player.credits == 100
+
     game._Game__award_bet(bet=100)
     assert game.player.credits == 300
 
@@ -38,10 +34,11 @@ def test_compute_round_result():
     seed(1)
     game = Game("test")
     assert game.player.credits == 100
+
     game.start_round()
     assert game._Game__current_card == Card("9", "D")
+
     result = game.compute_round_result(bet=100, prediction=Prediction.HIGHER)
-    assert isinstance(result, RoundResult)
     assert result.drawn_card == Card("J", "H")
     assert result.win
     assert not result.is_player_bankrupt
@@ -76,8 +73,8 @@ def test_is_win_invalid_prediction_raise_TypeError():
 def test_start_round():
     game = Game("test")
     game._Game__current_card = Card("A", "H")
+
     roundinfo = game.start_round()
-    assert isinstance(roundinfo, RoundInfo)
     assert roundinfo.player.name == "test"
     assert roundinfo.current_card == Card("A", "H")
 
@@ -86,8 +83,8 @@ def test_start_round_no_current_card():
     seed(1)
     game = Game("test")
     assert game._Game__current_card is None
+
     roundinfo = game.start_round()
-    assert isinstance(roundinfo, RoundInfo)
     assert roundinfo.player.name == "test"
     assert game._Game__current_card == Card("9", "D")
     assert roundinfo.current_card == Card("9", "D")
@@ -96,19 +93,22 @@ def test_start_round_no_current_card():
 def test_take_bet():
     game = Game("test")
     assert game.player.credits == 100
+
     game._Game__take_bet(bet=100)
     assert game.player.credits == 0
-
-
-def test_take_bet_less_than_zero_bet_raise_ValueError():
-    game = Game("test")
-    assert game.player.credits == 100
-    with pytest.raises(ValueError):
-        game._Game__take_bet(bet=-100)
 
 
 def test_take_bet_insufficient_credits_raise_ValueError():
     game = Game("test")
     assert game.player.credits == 100
+
     with pytest.raises(ValueError):
         game._Game__take_bet(bet=1000)
+
+
+def test_take_bet_less_than_zero_bet_raise_ValueError():
+    game = Game("test")
+    assert game.player.credits == 100
+
+    with pytest.raises(ValueError):
+        game._Game__take_bet(bet=-100)
