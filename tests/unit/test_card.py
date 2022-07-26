a42b64de6deea6d0874dcab005beca56230fb890
Bobotato <bhojwanialexander@gmail.com>
2022-07-26 17:13:28 +0800

    Improve code coverage

--- tests/unit/test_card.py
+++ tests/unit/test_card.py
@@ -1,4 +1,15 @@
+import pytest
+
 from hilo.models.card import Card, Ranks, Suits
+
+
+# DummyClass for testing eq against non-card object
+class DummyClass:
+    def __init__(self):
+        return
+
+    def __eq__(self, other):
+        raise TypeError
 
 
 def test_card_create():
@@ -15,6 +26,29 @@ def test_card_calculate_sort_index():
 
 def test_card_eq():
     assert Card.create(Ranks.A, Suits.D) == Card.create(Ranks.A, Suits.D)
+
+
+def test_card_eq_not_implemented():
+    with pytest.raises(TypeError):
+        Card.create(Ranks.A, Suits.D) == DummyClass()
+
+
+def test_card_gt():
+    assert Card.create(Ranks.TWO, Suits.D) > Card.create(Ranks.A, Suits.D)
+
+
+def test_card_gt_not_implemented():
+    with pytest.raises(TypeError):
+        Card.create(Ranks.A, Suits.D) > "test"
+
+
+def test_card_lt():
+    assert Card.create(Ranks.A, Suits.D) < Card.create(Ranks.TWO, Suits.D)
+
+
+def test_card_lt_not_implemented():
+    with pytest.raises(TypeError):
+        Card.create(Ranks.A, Suits.D) < "test"
 
 
 def test_card_str():
