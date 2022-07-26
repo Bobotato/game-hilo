3fe64e263fee7b9d330770649154a068ed7bb918
Bobotato <bhojwanialexander@gmail.com>
2022-07-26 22:50:25 +0800

    Shorten test

--- tests/unit/test_roundresult.py
+++ tests/unit/test_roundresult.py
@@ -5,11 +5,13 @@ from hilo.models.roundresult import RoundResult
 def test_round_result_init():
     roundresult = RoundResult(
         drawn_card=Card.create(Ranks.A, Suits.H),
-        win=True,
-        is_player_bankrupt=True,
-        is_deck_empty=True,
+        win=False,
+        is_player_bankrupt=False,
+        is_deck_empty=False,
     )
-    assert roundresult.drawn_card == Card.create(Ranks.A, Suits.H)
-    assert roundresult.win
-    assert roundresult.is_player_bankrupt
-    assert roundresult.is_deck_empty
+    assert roundresult == RoundResult(
+        drawn_card=Card.create(Ranks.A, Suits.H),
+        win=False,
+        is_player_bankrupt=False,
+        is_deck_empty=False,
+    )
