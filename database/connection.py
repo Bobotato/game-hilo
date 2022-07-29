4825d434c160b6f711d2dc590e5c56460f371568
Bobotato <bhojwanialexander@gmail.com>
2022-07-29 15:49:58 +0800

    Rearrange methods

--- database/connection.py
+++ database/connection.py
@@ -14,18 +14,6 @@ class DatabaseConnection:
         self.connection.commit()
         print(f"Added {username} as a new player!")
 
-    def is_returning_player(self, username: str) -> bool:
-        self.cursor.execute(
-            "SELECT * FROM users WHERE username = (%s);", (username,)
-        )
-
-        if not self.cursor.fetchone():
-            print("It looks like you're new.")
-            return False
-
-        print(f"Welcome back {username}.")
-        return True
-
     def connect_db(self):
         connection = psycopg2.connect(
             user="hilo",
@@ -46,7 +34,16 @@ class DatabaseConnection:
             "SELECT password FROM users WHERE username = (%s);", (username,)
         )
 
-        if password != self.cursor.fetchone()[0]:
+        return password == self.cursor.fetchone()[0]
+
+    def is_returning_player(self, username: str) -> bool:
+        self.cursor.execute(
+            "SELECT * FROM users WHERE username = (%s);", (username,)
+        )
+
+        if not self.cursor.fetchone():
+            print("It looks like you're new.")
             return False
 
+        print(f"Welcome back {username}.")
         return True
