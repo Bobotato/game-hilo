c654329ff725463cac79fbd179b6c6984e61d45d
Alexander Bhojwani <102455514+Bobotato@users.noreply.github.com>
2022-07-29 15:51:00 +0800

    Refactor dataclasses (#63)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,7 +3,10 @@ from random import seed
 import pytest
 
 from hilo.game import Game, Prediction
-from hilo.models.card import Card
+from hilo.models.card import Card, Ranks, Suits
+from hilo.models.player import Player
+from hilo.models.roundinfo import RoundInfo
+from hilo.models.roundresult import RoundResult
 
 
 @pytest.mark.integtest
@@ -14,10 +17,12 @@ def test_can_play_single_round():
     round_result = game.compute_round_result(
         prediction=Prediction.HIGHER, bet=50
     )
-    assert round_result.drawn_card == Card("J", "H")
-    assert round_result.win
-    assert not round_result.is_player_bankrupt
-    assert not round_result.is_deck_empty
+    assert round_result == RoundResult(
+        drawn_card=Card.create(Ranks.J, Suits.H),
+        win=True,
+        is_player_bankrupt=False,
+        is_deck_empty=False,
+    )
 
 
 @pytest.mark.integtest
@@ -33,10 +38,12 @@ def test_can_play_multiple_rounds():
     round_result = game.compute_round_result(
         prediction=Prediction.LOWER, bet=50
     )
-    assert round_result.drawn_card == Card("10", "S")
-    assert round_result.win
-    assert not round_result.is_player_bankrupt
-    assert not round_result.is_deck_empty
+    assert round_result == RoundResult(
+        drawn_card=Card.create(Ranks.TEN, Suits.S),
+        win=True,
+        is_player_bankrupt=False,
+        is_deck_empty=False,
+    )
 
 
 @pytest.mark.integtest
@@ -50,10 +57,12 @@ def test_can_play_subsequent_rounds_without_explicitly_starting_round():
     round_result = game.compute_round_result(
         prediction=Prediction.LOWER, bet=10
     )
-    assert round_result.drawn_card == Card("10", "S")
-    assert round_result.win
-    assert not round_result.is_player_bankrupt
-    assert not round_result.is_deck_empty
+    assert round_result == RoundResult(
+        drawn_card=Card.create(Ranks.TEN, Suits.S),
+        win=True,
+        is_player_bankrupt=False,
+        is_deck_empty=False,
+    )
 
 
 @pytest.mark.integtest
@@ -75,8 +84,10 @@ def test_can_start_round_again_without_playing():
     game = Game("test")
     round_info = game.start_round()
     round_info = game.start_round()
-    assert round_info.player.name == "test"
-    assert round_info.current_card == Card("9", "D")
+    assert round_info == RoundInfo(
+        player=Player(name="test"),
+        current_card=Card.create(Ranks.NINE, Suits.D),
+    )
 
 
 @pytest.mark.integtest
@@ -87,8 +98,10 @@ def test_can_start_round_again_after_playing():
     game.compute_round_result(prediction=Prediction.HIGHER, bet=50)
 
     round_info = game.start_round()
-    assert round_info.player.name == "test"
-    assert round_info.current_card == Card("J", "H")
+    assert round_info == RoundInfo(
+        player=Player(name="test", credits=150),
+        current_card=Card.create(Ranks.J, Suits.H),
+    )
 
 
 @pytest.mark.integtest
