f2bf43362506428e133d6a3da2f91aa767b58c28
Bobotato <bhojwanialexander@gmail.com>
2022-09-12 16:51:27 +0800

    Remove unneeded sentinel parameters

--- repository/user.py
+++ repository/user.py
@@ -8,7 +8,7 @@ from repository.models.user import User
 
 
 @DatabaseConnection.bind_connection
-def add_entry(*, cursor: psycopg2.extensions.cursor, user: User):
+def add_entry(cursor: psycopg2.extensions.cursor, user: User):
     """
     Adds a new user entry to the database
     :param user: The new player's user object.
@@ -19,6 +19,7 @@ def add_entry(*, cursor: psycopg2.extensions.cursor, user: User):
             "INSERT INTO users(username, password) VALUES (%s, %s);",
             (user.username, user.password_hash),
         )
+
     except psycopg2.errors.UniqueViolation:
         raise UsernameTakenException
 
@@ -27,11 +28,12 @@ def add_entry(*, cursor: psycopg2.extensions.cursor, user: User):
 
 
 @DatabaseConnection.bind_connection
-def get_entry(*, cursor: psycopg2.extensions.cursor, username: str) -> User:
+def get_entry(cursor: psycopg2.extensions.cursor, username: str) -> User:
     cursor.execute(
         "SELECT * FROM users WHERE username = (%s);",
         (username,),
     )
+
     try:
         _, username, password_hash = cursor.fetchone()
         return User(username, password_hash)
