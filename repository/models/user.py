aa57459d1e2a0cc4a3f6b120d18e844b1e9d56a1
Bobotato <bhojwanialexander@gmail.com>
2022-09-16 15:41:56 +0800

    Refactor into dataclass

--- repository/models/user.py
+++ repository/models/user.py
@@ -1,6 +1,7 @@
-class User:
-    """A class to represent a user and their login details"""
+from dataclasses import dataclass
 
-    def __init__(self, username: str, password_hash: str):
-        self.username = username
-        self.password_hash = password_hash
+
+@dataclass
+class User:
+    username: str
+    password_hash: str
