e3f42e37f57d54f078337ee3dd342bf102da0432
Bobotato <bhojwanialexander@gmail.com>
2023-02-06 16:42:44 +0800

    Add NoGameException

--- api/router/game/errors.py
+++ api/router/game/errors.py
@@ -1,3 +1,8 @@
+class NoGameException(Exception):
+    def __str__(self):
+        return "No game object can be found."
+
+
 class NoRoundInfoException(Exception):
     def __str__(self):
         return "No round info can be found."
