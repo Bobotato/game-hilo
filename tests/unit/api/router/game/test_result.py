c04da45357261f6d44333f967780a071be5ff376
Bobotato <bhojwanialexander@gmail.com>
2023-03-20 20:10:06 +0800

    Update test_result

--- tests/unit/api/router/game/test_result.py
+++ tests/unit/api/router/game/test_result.py
@@ -1,23 +1,15 @@
 from fastapi.testclient import TestClient
-from freezegun import freeze_time
+from jose import ExpiredSignatureError, JWTError
 
 from api.main import app
-from api.repository.pickler import pickle_object
+from api.repository.errors import NoSuchGameException
+from api.router import error_codes
 
 client = TestClient(app)
 
 
-class MockGame:
-    def __init__(self, **_):
-        pass
-
-    def start_round(self, **_):
-        return {
-            "player": {"name": "test", "credits": 100},
-            "current_card": {"sort_index": 1, "rank": 1, "suit": 2},
-        }
-
-    def compute_round_result(self, **_):
+def test_result(monkeypatch):
+    def mock_get_result(**_):
         return {
             "drawn_card": {"sort_index": 1, "rank": 1, "suit": 1},
             "win": True,
@@ -25,53 +17,9 @@ class MockGame:
             "is_deck_empty": False,
         }
 
-
-mock_game = MockGame()
-pickled_mock_game = pickle_object(mock_game)
-
-
-class MockUser:
-    def __init__(self, mock_game):
-        self.username = "test"
-        self.game = mock_game
-        self.round_info = "test"
-
-
-class MockUserNoGame:
-    def __init__(self):
-        self.username = "test"
-        self.game = None
-        self.round_info = "testroundinfo"
-
-
-class MockUserNoRoundInfo:
-    def __init__(self):
-        self.username = "test"
-        self.game = "testgame"
-        self.round_info = None
-
-
-def mock_update_game(**_):
-    return "updated"
-
-
-def mock_update_round_info(**_):
-    return "updated"
-
-
-@freeze_time("2000-01-01")
-def test_result(monkeypatch):
-    def mock_get_user_from_token_return_MockUser(**_):
-        return MockUser(mock_game=pickled_mock_game)
-
     monkeypatch.setattr(
-        "api.router.game.game.get_user_from_token",
-        mock_get_user_from_token_return_MockUser,
-    )
-
-    monkeypatch.setattr("api.router.game.game.update_game", mock_update_game)
-    monkeypatch.setattr(
-        "api.router.game.game.update_round_info", mock_update_round_info
+        "api.router.game.game.get_result",
+        mock_get_result,
     )
 
     response = client.post(
@@ -90,14 +38,13 @@ def test_result(monkeypatch):
     }
 
 
-@freeze_time("2000-01-01")
-def test_result_missing_game_return_HTTPException(monkeypatch):
-    def mock_get_user_from_token_return_None(**_):
-        return MockUserNoGame()
+def test_result_expired_token_return_401(monkeypatch):
+    def mock_get_result_raise_ExpiredSignatureError(**_):
+        raise ExpiredSignatureError
 
     monkeypatch.setattr(
-        "api.router.game.game.get_user_from_token",
-        mock_get_user_from_token_return_None,
+        "api.router.game.game.get_result",
+        mock_get_result_raise_ExpiredSignatureError,
     )
 
     response = client.post(
@@ -107,20 +54,20 @@ def test_result_missing_game_return_HTTPException(monkeypatch):
         },
     )
 
-    assert response.status_code == 400
+    assert response.status_code == 401
     assert response.json() == {
-        "detail": "There is no game associated with this player."
+        "error_code": error_codes.EXPIRED_TOKEN,
+        "detail": "The token has expired. Please login again.",
     }
 
 
-@freeze_time("2000-01-01")
-def test_result_missing_round_info(monkeypatch):
-    def mock_get_user_from_token_no_round_info(**_):
-        return MockUserNoRoundInfo()
+def test_result_invalid_token_return_401(monkeypatch):
+    def mock_get_result_raise_JWTError(**_):
+        raise JWTError
 
     monkeypatch.setattr(
-        "api.router.game.game.get_user_from_token",
-        mock_get_user_from_token_no_round_info,
+        "api.router.game.game.get_result",
+        mock_get_result_raise_JWTError,
     )
 
     response = client.post(
@@ -130,7 +77,31 @@ def test_result_missing_round_info(monkeypatch):
         },
     )
 
-    assert response.status_code == 403
+    assert response.status_code == 401
     assert response.json() == {
-        "detail": "Round info does not exist, start a round first."
+        "error_code": error_codes.INVALID_TOKEN,
+        "detail": "The given token is invalid.",
+    }
+
+
+def test_result_no_such_game_returns_404(monkeypatch):
+    def mock_get_result_raise_NoSuchGameException(**_):
+        raise NoSuchGameException
+
+    monkeypatch.setattr(
+        "api.router.game.game.get_result",
+        mock_get_result_raise_NoSuchGameException,
+    )
+
+    response = client.post(
+        "/game/result?bet=50&prediction=1",
+        json={
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjo5NDY2ODYwMDB9.CR_OzFi2WTQN7Y02eQXubB3rVRwyLv4JmxpkeV8vpas"  # noqa: E501
+        },
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "error_code": error_codes.NO_SUCH_GAME,
+        "detail": "There is no game associated with this user.",
     }
