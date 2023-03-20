3b3127612ddf70f50cc870af1e47c752629ac380
Bobotato <bhojwanialexander@gmail.com>
2023-03-20 19:43:29 +0800

    Update test_info

--- tests/unit/api/router/game/test_info.py
+++ tests/unit/api/router/game/test_info.py
@@ -1,69 +1,22 @@
 from fastapi.testclient import TestClient
 from freezegun import freeze_time
+from jose import ExpiredSignatureError, JWTError
 
 from api.main import app
-from api.repository.pickler import pickle_object
+from api.router import error_codes
 
 client = TestClient(app)
 
 
-class MockGame:
-    def __init__(self, **_):
-        pass
-
-    def start_round(self, **_):
+@freeze_time("2000-01-01")
+def test_info(monkeypatch):
+    def mock_get_info(**_):
         return {
             "player": {"name": "test", "credits": 100},
             "current_card": {"sort_index": 1, "rank": 1, "suit": 2},
         }
 
-
-mock_game = MockGame()
-pickled_mock_game = pickle_object(mock_game)
-
-
-class MockUser:
-    def __init__(self, mock_game):
-        self.username = "test"
-        self.game = mock_game
-
-
-class MockUserNoGame:
-    def __init__(self):
-        self.username = "test"
-        self.game = None
-        self.round_info = "testroundinfo"
-
-
-class MockUserNoRoundInfo:
-    def __init__(self):
-        self.username = "test"
-        self.game = "testgame"
-        self.round_info = None
-
-
-def mock_update_game(**_):
-    return "updated"
-
-
-def mock_update_round_info(**_):
-    return "updated"
-
-
-@freeze_time("2000-01-01")
-def test_info(monkeypatch):
-    def mock_get_user_from_token_return_MockUser(**_):
-        return MockUser(mock_game=pickled_mock_game)
-
-    monkeypatch.setattr(
-        "api.router.game.game.get_user_from_token",
-        mock_get_user_from_token_return_MockUser,
-    )
-
-    monkeypatch.setattr("api.router.game.game.update_game", mock_update_game)
-    monkeypatch.setattr(
-        "api.router.game.game.update_round_info", mock_update_round_info
-    )
+    monkeypatch.setattr("api.router.game.game.get_info", mock_get_info)
 
     response = client.post(
         "/game/info",
@@ -79,22 +32,14 @@ def test_info(monkeypatch):
     }
 
 
-@freeze_time("2000-01-01")
-def test_info_no_game(monkeypatch):
-    def mock_get_user_from_token_return_MockUserNoGame(**_):
-        return MockUserNoGame()
+def test_info_expired_signature_returns_401(monkeypatch):
+    def mock_get_info_raises_Expired_Signature_Error(**_):
+        raise ExpiredSignatureError
 
     monkeypatch.setattr(
-        "api.router.game.game.get_user_from_token",
-        mock_get_user_from_token_return_MockUserNoGame,
+        "api.router.game.game.get_info",
+        mock_get_info_raises_Expired_Signature_Error,
     )
-
-    monkeypatch.setattr("api.router.game.game.update_game", mock_update_game)
-    monkeypatch.setattr(
-        "api.router.game.game.update_round_info", mock_update_round_info
-    )
-
-    monkeypatch.setattr("api.router.game.game.Game", MockGame)
 
     response = client.post(
         "/game/info",
@@ -103,8 +48,31 @@ def test_info_no_game(monkeypatch):
         },
     )
 
-    assert response.status_code == 200
+    assert response.status_code == 401
     assert response.json() == {
-        "player": {"name": "test", "credits": 100},
-        "current_card": {"sort_index": 1, "rank": 1, "suit": 2},
+        "error_code": error_codes.EXPIRED_TOKEN,
+        "detail": "The token has expired. Please login again.",
+    }
+
+
+def test_info_JWT_error_returns_401(monkeypatch):
+    def mock_get_info_raises_JWTError(**_):
+        raise JWTError
+
+    monkeypatch.setattr(
+        "api.router.game.game.get_info",
+        mock_get_info_raises_JWTError,
+    )
+
+    response = client.post(
+        "/game/info",
+        json={
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjo5NDY2ODYwMDB9.CR_OzFi2WTQN7Y02eQXubB3rVRwyLv4JmxpkeV8vpas"  # noqa: E501
+        },
+    )
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "error_code": error_codes.INVALID_TOKEN,
+        "detail": "The given token is invalid.",
     }
