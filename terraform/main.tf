05591db349d3f92e27b3db059aa68c359829e31a
Bobotato <bhojwanialexander@gmail.com>
2023-11-16 01:51:41 +0800

    test comment action

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "digitalocean_droplet" "hilo-game" {
   }
 
     provisioner "local-exec" {
-                command = "until nc -zv '${self.ipv4_address}' 22; do sleep 1; done; echo 'SSH port open' && ansible-playbook -u root -i '${self.ipv4_address},' --private-key '${var.pvt_key}' -e ./database_details.yml ../playbook.yml"
+                command = "until nc -zv '${self.ipv4_address}' 22; do sleep 1; done; echo 'SSH port open' && ansible-playbook -u root -i '${self.ipv4_address},' --private-key '${var.pvt_key}' ../playbook.yml"
 
     }
 }
